c3f3a23a1b8c1c3d850ede37b7ebd08ae0bf1c67
Stelele <giftmugweni@gmail.com>
2026-01-08 21:15:55 +0200

    added padding at the bottom

--- frontend/src/layouts/BasePage.vue
+++ frontend/src/layouts/BasePage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mt-16 w-full h-full overflow-scroll lg:overflow-hidden">
+    <div class="mt-16 w-full h-full px-2 pt-2 pb-20 overflow-scroll">
         <slot />
     </div>
 </template>
