52f03b19e83714f38682354480ad47408ff43751
Gift Mugweni <giftmugweni@gmail.com>
2026-04-22 00:15:08 +0200

    triggering build

--- backend/Api/Endpoints/Endpoints/CompanyEndpoints.cs
+++ backend/Api/Endpoints/Endpoints/CompanyEndpoints.cs
@@ -14,8 +14,6 @@ public static class CompanyEndpoints
         {
             var query = new GetCompaniesQuery(companyIds);
             var result = await mediator.Send(query);
-
-            var test = 1;
             return Results.Ok(result);
         })
         .WithTags(Tags.Companies)
